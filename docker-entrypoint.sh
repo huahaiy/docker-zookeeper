8fc5dfbf633a75a25c70256e294f10b72021c65f
Huahai Yang <huahai.yang@gmail.com>
2015-03-09 21:44:08 -0700

    bind to docker internal IP for self node

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,7 +16,11 @@ if [ "$1" = 'start-foreground' ]; then
   for VAR in `env`; do
     if [[ $VAR =~ ^ZOO_IP_ ]]; then
       n=`echo $VAR | sed -r "s/ZOO_IP_(.*)=.*/\1/g"`
-      ip=`echo $VAR | sed -r "s/.*=(.*)/\1/g"`
+      if [ $n -eq $ZOO_NODE_ID ]; then
+        ip=`hostname -i`
+      else
+        ip=`echo $VAR | sed -r "s/.*=(.*)/\1/g"`
+      fi
       echo "server.$n=$ip:2888:3888" >> /opt/zookeeper-3.4.6/conf/zoo.cfg
     fi
   done
